4e38e2a60ab85f425c9b31a6408853a23fd952ba
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2018-01-25 05:00:36 -0800

    Update :S

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
-!function(e){"use strict";var r=e.querySelector("#app");r.displayInstalledToast=function(){e.querySelector("#caching-complete").show()},r.addEventListener("dom-change",function(){console.log("Our app is ready to rock!")}),window.addEventListener("WebComponentsReady",function(){e.querySelector("body").removeAttribute("unresolved");var r=e.querySelector("#paperDrawerPanel");r.forceNarrow=!0}),r.onMenuSelect=function(){var r=e.querySelector("#paperDrawerPanel");r.narrow&&r.closeDrawer()}}(document);
+!function (e) { 
+    "use strict";
+     var r = e.querySelector("#app"); 
+    r.displayInstalledToast = function () {
+        e.querySelector("#caching-complete").show() 
+    },
+    r.addEventListener("dom-change",
+    function () { 
+        console.log("Our app is ready to rock!")
+    }),
+    window.addEventListener("WebComponentsReady", 
+    function () {
+        e.querySelector("body").removeAttribute("unresolved"); 
+        var r = e.querySelector("#paperDrawerPanel"); 
+        r.forceNarrow = !0 
+    }),
+    r.onMenuSelect = function () { 
+    var r = e.querySelector("#paperDrawerPanel"); 
+    r.narrow && r.closeDrawer() 
+    }
+}(document);
